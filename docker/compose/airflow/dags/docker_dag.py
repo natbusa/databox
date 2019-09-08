b643e3a980b285dba219825b12e043fa3639c845
natbusa <natalino.busa@gmail.com>
2019-09-08 13:07:07 +0800

    added airflow

---
+++ docker/compose/airflow/dags/docker_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+        'owner'                 : 'airflow',
+        'description'           : 'Use of the DockerOperator',
+        'depend_on_past'        : False,
+        'start_date'            : datetime(2018, 1, 3),
+        'email_on_failure'      : False,
+        'email_on_retry'        : False,
+        'retries'               : 1,
+        'retry_delay'           : timedelta(minutes=5)
+}
+
+with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *", catchup=False) as dag:
+        t2 = DockerOperator(
+                task_id='docker_command',
+                image='centos:latest',
+                api_version='auto',
+                auto_remove=True,
+                command="/bin/sleep 1",
+                docker_url="unix://var/run/docker.sock",
+                network_mode="bridge"
+        )
+
+        t3 = BashOperator(
+                task_id='print_hello',
+                bash_command='echo "hello world"'
+        )
+
+        t2 >> t3
+
---
+++ docker/compose/airflow/dags/docker_git_spark.py
@@ -0,0 +1,69 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.docker_operator import DockerOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+import os.path
+
+default_args = {
+    'owner'         : 'airflow',
+    'description'       : 'Use of the DockerOperator',
+    'depend_on_past'    : False,
+    'start_date'        : datetime(2018, 1, 3),
+    'email_on_failure'      : False,
+    'email_on_retry'    : False,
+    'retries'           : 1,
+    'retry_delay'       : timedelta(minutes=5)
+}
+
+def checkIfRepoIsAlreadyCloned():
+    if os.path.exists('/home/airflow/etl/tutorial'):
+        return 'dummy'
+    return 'git_clone'
+
+with DAG(
+    'docker_git_spark',
+    default_args=default_args,
+    schedule_interval="5 * * * *",
+    catchup=False) as dag:
+
+    t_git_clone = BashOperator(
+        task_id='git_clone',
+        bash_command='git clone https://github.com/natbusa/dlf-tutorial /home/airflow/etl/tutorial'
+    )
+
+    t_git_pull = BashOperator(
+        task_id='git_pull',
+        bash_command='cd /home/airflow/etl/tutorial && git pull',
+        trigger_rule='one_success'
+    )
+
+    t_check_repo = BranchPythonOperator(
+        task_id='does_repo_exist',
+        python_callable=checkIfRepoIsAlreadyCloned
+    )
+
+    t_dummy = DummyOperator(
+        task_id='dummy'
+    )
+
+    t_check_repo >> t_git_clone >> t_git_pull
+    t_check_repo >> t_dummy >> t_git_pull
+
+    t_docker = DockerOperator(
+        task_id='docker_command',
+        image='natbusa/pyspark-notebook:2.4.4-hadoop-3.2.0',
+        api_version='auto',
+        auto_remove=True,
+        environment={
+        },
+        volumes=['/home/airflow/etl/tutorial:/etl/tutorial'],
+        command='spark-submit --master spark://spark-master:7077 /etl/tutorial/minimal.py',
+        docker_url='unix://var/run/docker.sock',
+        network_mode='bridge'
+    )
+
+    t_git_pull >> t_docker
+
