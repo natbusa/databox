35d2cb83de1670e72c56147c97d8921869c454db
natbusa <natalino.busa@gmail.com>
2019-09-09 10:51:06 +0800

    deleted config, moved to containers

--- docker/compose/jupyter/config/spark-env.sh
+++
@@ -1,4 +0,0 @@
-### in conf/spark-env.sh ###
-
-# If $HADOOP_HOME is defined and points to the hadoop homedir installation
-export SPARK_DIST_CLASSPATH=$SPARK_HOME/jars/*:$(${HADOOP_HOME}/bin/hadoop classpath)
