57995285547130f76eba359c40eea667e4ddafcb
natbusa <natalino.busa@gmail.com>
2019-10-08 15:43:13 +0800

    added redash

---
+++ docker/compose/redash/setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# This script setups dockerized Redash on Ubuntu 18.04.
+set -eu
+
+REDASH_BASE_PATH=./
+
+create_config() {
+    if [[ -e $REDASH_BASE_PATH/env ]]; then
+        rm $REDASH_BASE_PATH/env
+        touch $REDASH_BASE_PATH/env
+    fi
+
+    COOKIE_SECRET=$(pwgen -1s 32)
+    SECRET_KEY=$(pwgen -1s 32)
+    POSTGRES_PASSWORD=$(pwgen -1s 32)
+    REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@redash-postgres/postgres"
+
+    echo "PYTHONUNBUFFERED=0" >> $REDASH_BASE_PATH/env
+    echo "REDASH_LOG_LEVEL=INFO" >> $REDASH_BASE_PATH/env
+    echo "REDASH_REDIS_URL=redis://redash-redis:6379/0" >> $REDASH_BASE_PATH/env
+    echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
+    echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
+    echo "REDASH_SECRET_KEY=$SECRET_KEY" >> $REDASH_BASE_PATH/env
+    echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
+}
+
+redash_create_db() {
+    docker-compose run --rm redash-server create_db
+}
+
+create_config
