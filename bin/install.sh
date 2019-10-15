04f1898366be3a4e6f07fd0a0aa7805c36bc8d62
natbusa <natalino.busa@gmail.com>
2019-10-16 01:44:16 +0800

    no colors

--- bin/install.sh
+++ bin/install.sh
@@ -2,31 +2,28 @@
 SCRIPT_DIR="$(dirname "$(realpath -s "${BASH_SOURCE}")")"
 ROOT_DIR="$(realpath "${ROOT_DIR:=${SCRIPT_DIR}/..}")"
 
-source "${SCRIPT_DIR}/colorize.sh"
-
 ################### PRE-REQUIREMENTS ###################
 function install_requirements {
   if (which make && which curl && which git) > /dev/null; then
-    echo "${BASH_SOURCE}: ${green}make, curl, git${normal}: already installed"
+    echo "${BASH_SOURCE}: make, curl, git: already installed"
     return
   fi
 
   pre_reqs=(apt-transport-https ca-certificates make curl git)
 
-  echo "${BASH_SOURCE}: Installing ${green}pre-requirement${normal}: ${pre_reqs[@]}..."
+  echo "${BASH_SOURCE}: Installing pre-requirement: ${pre_reqs[@]}..."
   apt-get update -qq > /dev/null
   apt-get install -y -qq ${pre_reqs[@]}
-  echo "${green}pre-requirement${normal} is installed"
 }
 install_requirements
 
 ################### DOCKER ###################
 function install_docker {
   if [ "$(which docker)" ]; then
-    echo "${BASH_SOURCE}: ${green}docker${normal}: already installed"
+    echo "${BASH_SOURCE}: docker: already installed"
     return
   fi
-  echo "${BASH_SOURCE}: Installing ${green}docker${normal}..."
+  echo "${BASH_SOURCE}: Installing docker..."
   curl -sSL get.docker.io | bash
   usermod -aG docker "$USER"
 }
@@ -37,19 +34,11 @@ DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.22.0}
 
 function install_docker_compose {
   if [ "$(which docker-compose)" ]; then
-      echo "${BASH_SOURCE}: ${green}docker-compose${normal}: already installed"
+      echo "${BASH_SOURCE}: docker-compose: already installed"
       return
   fi
-  echo "${BASH_SOURCE}: Installing ${green}docker-compose${normal}..."
+  echo "${BASH_SOURCE}: Installing docker-compose..."
   curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
 install_docker_compose
-
-################### BUILD CONTAINERS ###################
-pushd ${ROOT_DIR}/docker/containers > /dev/null
-for c in $(ls)
-do
-  pushd $c && make build && popd
-done
-popd > /dev/null
