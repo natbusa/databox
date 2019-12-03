45d85111b82cff7602f54d2d4de925901fc2eaef
natbusa <natalino.busa@gmail.com>
2019-12-03 16:35:58 +0800

    deleted files (moved to datafaucet examples)

--- demos/oasis/demo/datafaucet/minimal.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = ( SparkSession
-          .builder
-          .appName("Python Spark SQL basic example")
-          .getOrCreate())
-
-print(spark.version)
